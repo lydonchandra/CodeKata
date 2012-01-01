3ee227a2021f70564ca3f4b877ac10e02137291f
unknown <lchandra@.(none)>
2012-01-01 12:15:22 +0900

    tidy up CodeKata1.java, added comments etc

--- src/main/java/ch/lydon/codekata/CodeKata1.java
+++ src/main/java/ch/lydon/codekata/CodeKata1.java
@@ -5,23 +5,32 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+/*
+ * From Data Structures and Algorithm in Java, 5th Edition
+ * C-1.4 Write a short Java program that outputs all possible strings formed by using the characters c,a,r,b,o,n exactly once
+ */
 public class CodeKata1 {
+	
 	public static void main(String [] args) {
 		
 		CodeKata1 codeKata1 = new CodeKata1();
 		
-//		codeKata1.perm1("dona");
+		// permutation of a simple string, results displayed on standard output
+		codeKata1.perm1Recursion("dona");
 		
+
+		// permutation of a simple string, results returned on a List
 		List<String> stringList = new ArrayList<String>();
-//		codeKata1.perm1List("dona", stringList);
+		codeKata1.perm1ListRecursion("dona", stringList);
+
 		
+		// permutation of a List of Character using recursion, result returned on a List of Character List
 		List<List<Character>> charListList = new ArrayList<List<Character>>();
-//		codeKata1.perm2(Arrays.asList('d','o', 'n', 'a'), charListList);
-		
-//		System.out.println(stringList.size());
-		//List< List<Character> > permute1 = codeKata1.permuteChar( Arrays.asList( 'c', 'a', 'r' ) );		
-		//codeKata1.printListListChar( permute1 );
-		
+		codeKata1.perm2ListRecursion(Arrays.asList('d','o', 'n', 'a'), charListList);
+		codeKata1.printListListChar(charListList);
+		System.out.println("------------------------");
+		 
+		// permutation of a List of Character not using RECURSION, results returned on a List of Character List
 		charListList = new ArrayList<List<Character>>();
 		codeKata1.permNoRecursion(Arrays.asList('d','o', 'n', 'a'), charListList);
 		System.out.println(charListList.size());
@@ -29,57 +38,59 @@ public class CodeKata1 {
 		
 	}
 	
-	public void printListListChar(List <List<Character>> charListList) {
-		
-		for( List<Character> charList: charListList) {
-			String tempString = "";
-			for( Character character: charList) {
-				tempString += character;
-			}
-			System.out.println(tempString);
-		}
-		
-	}
+	
+	
 
 	
 	
 	
-	
+	/*
+	 * permutation of a List of Character NOT using RECURSION, results returned on a List of Character List
+	 * 
+	 * The algorithm used for permutations of the string is an adaptation of the "Countdown Quickperm algorithm"
+	 * by Mr. Phillip Paul Fuchs. The credit for this algo goes to him. http://www.quickperm.org/01example.php
+	 * 
+	 * the C version is at http://www.daniweb.com/software-development/c/code/216767
+	 */
 	public void permNoRecursion(List<Character> inCharList,  List< List<Character>> outListList ) {
 		
 		int stringLength = inCharList.size();
 		
-		int p[] = new int[ stringLength+1 ];
+		int p[] = new int[ stringLength+1 ]; // p is used to control the iteration
 		
 		for( int j = 0; j <= stringLength; j++) {
-			p[j] = j;
+			
+			p[j] = j;	// init p[1] to 1, p[2] to 2 and so on
 		}
 		
-//		List<List<Character>> outCharList = new ArrayList<List<Character>>();
-
 		List<Character> tempInCharList = new ArrayList<Character>(stringLength);
 		tempInCharList.addAll(inCharList);
 		
 		int counter = 0;
 		int i = 1, j = 0;
+		
 		while( i < stringLength ) {
 			
 			p[i]--;
-			j = i % 2 * p[i];
+			j = i % 2 * p[i];  // if i is odd then j = p[i] otherwise j = 0
 			
+			// swap tempInCharList[i] and tempInCharList[j]
 			Character tempChar = tempInCharList.get(i);
 			tempInCharList.set(i, tempInCharList.get(j));
 			tempInCharList.set(j, tempChar);
 			
 			counter++;
 			
+			// create a new temp List so we can copy it to our output List outListList
 			List<Character> tempInCharListCopy = new ArrayList<Character>();
 			tempInCharListCopy.addAll( tempInCharList );
 			outListList.add(tempInCharListCopy);
 			
+			
 			i = 1;
 			while( p[i] == 0) {
-				p[i] = i;
+				
+				p[i] = i;	// reset p[i] zero value
 				i++;
 			}
 			
@@ -88,30 +99,36 @@ public class CodeKata1 {
 	
 	
 	
-	/*
-	 * C-1.4 Write a short Java program that outputs all possible strings formed by using the characters c,a,r,b,o,n exactly once
+	/**
+	 * The algorithm is from 
+	 * http://introcs.cs.princeton.edu/java/23recursion/Permutations.java.html
+	 *
 	 */
-	public void perm1(String s) {
-		perm1( "", s);
+	public void perm1Recursion(String s) {
+		perm1Recursion( "", s);
 	}
-	public void perm1(String prefix, String s) {
+	public void perm1Recursion(String prefix, String s) {
 		int N = s.length();
 		if( N == 0) 
 			System.out.println(prefix);
 		else
 		{
 			for (int i=0; i<N; i++) {
-				perm1( prefix + s.charAt(i), s.substring(0,i) + s.substring(i+1,N));
+				perm1Recursion( prefix + s.charAt(i), s.substring(0,i) + s.substring(i+1,N));
 			}
 		}
 	}
 	
 	
-	
-	public void perm1List(String s, List<String> outList) {
-		perm1List( "", s, outList);
+	/**
+	 * The algorithm is an adaptation of 
+	 * http://introcs.cs.princeton.edu/java/23recursion/Permutations.java.html
+	 *
+	 */
+	public void perm1ListRecursion(String s, List<String> outList) {
+		perm1ListRecursion( "", s, outList);
 	}
-	public void perm1List(String prefix, String s, List<String> outList) {
+	public void perm1ListRecursion(String prefix, String s, List<String> outList) {
 		int N = s.length();
 		if( N == 0) {
 			outList.add(prefix);
@@ -120,18 +137,22 @@ public class CodeKata1 {
 		else
 		{
 			for (int i=0; i<N; i++) {
-				perm1List( prefix + s.charAt(i), s.substring(0,i) + s.substring(i+1,N), outList);
+				perm1ListRecursion( prefix + s.charAt(i), s.substring(0,i) + s.substring(i+1,N), outList);
 			}
 		}
 	}
 	
 	
-	
-	public void perm2( List<Character> s, List< List<Character>> outListList ) {
+	/**
+	 * The algorithm is an adaptation of 
+	 * http://introcs.cs.princeton.edu/java/23recursion/Permutations.java.html
+	 *
+	 */
+	public void perm2ListRecursion( List<Character> s, List< List<Character>> outListList ) {
 		List<Character> emptyCharList = Collections.emptyList();
-		perm2ListList( emptyCharList, s, outListList);
+		perm2ListRecursion( emptyCharList, s, outListList);
 	}	
-	public void perm2ListList( List<Character> prefix, List<Character> s, List< List<Character>> outListList ) {
+	public void perm2ListRecursion( List<Character> prefix, List<Character> s, List< List<Character>> outListList ) {
 		int N = s.size();
 		if( N == 0 ) {
 			outListList.add( prefix );
@@ -142,13 +163,14 @@ public class CodeKata1 {
 				
 				// created both param1List and param2List because we can't modify the list returned by subList()
 				List<Character> param1List = new ArrayList<Character>();
+				param1List.addAll(prefix);
 				param1List.add(s.get(i));
 				
 				List<Character> param2List = new ArrayList<Character>();
 				param2List.addAll(s.subList(0,i));
 				param2List.addAll(s.subList(i+1, N));				
 				
-				perm2ListList( param1List, param2List , outListList );
+				perm2ListRecursion( param1List, param2List , outListList );
 			}
 			
 		}
@@ -156,5 +178,21 @@ public class CodeKata1 {
 	
 	
 	
+	/*
+	 * Print the content of List< List<Character> >
+	 */
+	public void printListListChar(List <List<Character>> charListList) {
+		
+		for( List<Character> charList: charListList) {
+
+			// temp string to concat all character in our List<Character>
+			String tempString = "";
+			for( Character character: charList) {
+				tempString += character;
+			}
+			System.out.println(tempString);
+		}
+		
+	}
 	
 }
